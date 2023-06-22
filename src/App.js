0341cc452aaf2450b9854a83c6acc097d353bb55
Aayush Asthana <aayushasthana@Aayushs-MacBook-Air.local>
2023-06-22 01:52:58 -0700

    added middle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Top from './components/top'
+import Middle from './components/middle'
 
 function App() {
   return (
     <div className="App">
         <Top></Top>
+        <Middle></Middle>
       
     </div>
   );
